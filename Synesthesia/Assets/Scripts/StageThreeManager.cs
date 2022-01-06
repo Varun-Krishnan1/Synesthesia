9c1ef12a5a08df572cde15aef8c937a7dcc9c1b0
Varun <2017.vkrishnan@gmail.com>
2022-01-05 21:30:38 -0500

    Working on detaching drumsticks from parent

--- Synesthesia/Assets/Scripts/StageThreeManager.cs
+++ Synesthesia/Assets/Scripts/StageThreeManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class StageThreeManager : MonoBehaviour
 {
+    public ActionBasedController[] controllers; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(ActionBasedController controller in controllers)
+        {
+            DrumStick drumstick = controller.gameObject.GetComponentsInChildren<DrumStick>()[0];
+            drumstick.transform.parent.gameObject.transform.parent = null; // -- unparent from controller 
+            // -- Add hands back onto controller (shouldn't destroy them in first place just set them inactive in Stage 0) 
+            // -- TODO: Add box collider and rigid body to drumstick 
+
+        }
     }
 
     // Update is called once per frame
